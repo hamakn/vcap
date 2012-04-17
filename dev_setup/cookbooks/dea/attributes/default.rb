95845a83500e1099fd3cb78b36ba95415d13756d
Katsunori Kawaguchi <hamakata@gmail.com>
2012-04-17 14:59:02 +0900

    Set secure = true on dea

--- dev_setup/cookbooks/dea/attributes/default.rb
+++ dev_setup/cookbooks/dea/attributes/default.rb
@@ -2,6 +2,6 @@ default[:dea][:config_file] = "dea.yml"
 default[:dea][:local_route] = nil
 default[:dea][:runtimes] = ["ruby18", "ruby19", "node04", "node06", "java", "erlang", "php", "python2"]
 default[:dea][:logging] = 'debug'
-default[:dea][:secure] = false
+default[:dea][:secure] = true
 default[:dea][:multi_tenant] = true
 default[:dea][:enforce_ulimit] = false
